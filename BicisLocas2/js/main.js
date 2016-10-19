b6816e91544e802ee2b9f9a55e19d3ed7adaff78
EdithRamirez <ediths.ramirez07@gmail.com>
2016-10-19 15:18:41 -0500

    agrege más comentarios a codigo js

--- BicisLocas2/js/main.js
+++ BicisLocas2/js/main.js
@@ -10,24 +10,26 @@ function validateForm(){
 	var spans = document.getElementsByClassName("error");
 	//
 	while(spans.length > 0){
-		spans[0].parentElement.removeChild(spans[0]);
+		spans[0].parentElement.removeChild(spans[0]); //se posiciona en span y los remueve dependiendo del parent
 	}
 
+	//declare variables que voy a utilizar en mi codigo
 	var nombre = document.getElementById("name");
 	var apellido = document.getElementById("lastname");
+
+	//expresion regular para validar correo
 	var regexCorreo = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 	var correo = document.getElementById("input-email");
 	var contrasena = document.getElementById("input-password");
 	var tipoBici = document.getElementsByTagName("select")[0];
 	
-	//NOMBRE
+	//Empieza a validar cada input y muestra los span
+	//NOMBRE 
 	if(nombre.value == null || nombre.value == ""){
 		span.innerHTML = "Debes ingresar nombre";
 		nombre.parentNode.appendChild(span);
 		return false;
 	}
-
-
 	else if(!/^[ÑA-Z][ña-z]*$/g.test(nombre.value)){
 		span.innerHTML = "La primera letra debe ser mayúscula";
 		nombre.parentNode.appendChild(span);
